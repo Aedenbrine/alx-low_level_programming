048191d3419e095d8ac09fb197cf4e5edc757848
Adeyinka <belloadeyinka611@gmail.com>
2022-12-09 05:04:06 -0800

    shege

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,22 +3,22 @@
 #include <time.h>
 
 /**
- *  * main - Prints base 10 numbers
- *   * @void: Empty parameter list for main.
- *    *
- *     * Description: Prints numbers 0-9
- *      *
- *       * Return: 0 for success
- *       */
+ * main - Prints base 10 numbers
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints numbers 0-9
+ *
+ * Return: 0 for success
+ */
 int main(void)
 {
-		char num = '0';
+	char num = '0';
 
-			while (num <= '9')
-					{
-								putchar(num);
-										num++;
-					}
-				putchar('\n');
-					return (0);
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }
