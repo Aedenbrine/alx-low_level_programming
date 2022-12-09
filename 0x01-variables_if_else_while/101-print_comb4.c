fe07869ff85284f079f19a5c9cf4aadf728305d1
Adeyinka <belloadeyinka611@gmail.com>
2022-12-09 14:48:15 -0800

    shege

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,9 +21,15 @@ int main(void)
 					putchar(n);
 					putchar(m);
 					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+								
 			}
-		}			
+		}
 	}
 	putchar(',');
 	return (0);
