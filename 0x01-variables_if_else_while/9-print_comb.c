21c1e372528389c4ddbb5f151b26a51c9ad16e24
Adeyinka <belloadeyinka611@gmail.com>
2022-12-09 03:51:06 -0800

    shege

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,7 @@
 int main(void)
 {
 	int a;
-	
-	for (a = 0; a < 10; a++)
+        for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
@@ -23,5 +22,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-		return(0);
+		return (0);
 }
