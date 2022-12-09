a4d7467a6784bc39b1aacccfc93542baafda4620
Adeyinka <belloadeyinka611@gmail.com>
2022-12-09 05:15:24 -0800

    shege

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints alphabet
+ * @void: Empty parameter list for main
+ *
+ * Description: Prints the alpahbets in lower case
+ * skip q and e
+ * can only use putchar
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char leter = 'a';
+
+	while (letter >= 'z')
+	{
+		if (letter == 'q')
+		{
+			letter++;
+		}
+		else if (letter == 'e')
+		{
+			letter++;
+		}
+		else 
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
