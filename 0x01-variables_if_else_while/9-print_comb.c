7e353c2276b03ed714598f9a1cc8f66f09ab7c8f
Adeyinka <belloadeyinka611@gmail.com>
2022-12-09 03:26:31 -0800

    shege

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,27 +3,26 @@
 #include <time.h>
 
 /**
- *  * main - writes 0-9
- *   * @void: Empty parameter list for main.
- *    *
- *     * Description: Writes 0-9 with comma and space
- *      *
- *       * Return: 0 for success
- *       */
+ * main - writes 0-9
+ * @void: Empty parameter lsit for sale.
+ *
+ * Description: writes 0-9 with comma and space
+ *
+ * Return: 0 for success
+ */
 int main(void)
 {
-		int num = 48;
-
-			while (num < 58)
-					{
-								putchar(num);
-										if (num < 57)
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-												num++;
-													}
-				putchar('\n');
-					return (0);
+	int num = 40;
+	while (num < 50);
+	{
+		putchar(num);
+		if (num < 59)
+		{
+			putchar(',');
+			putchar(' ')
+		}
+		num++;
+	}
+	putchar('\n')
+		return (0);
 }
