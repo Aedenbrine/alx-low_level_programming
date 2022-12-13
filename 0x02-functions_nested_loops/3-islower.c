54cf8b5d4ad8248794c80d9b33559ea57028f27a
Adeyinka <belloadeyinka611@gmail.com>
2022-12-13 04:24:24 -0800

    shege

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _islower - Check a lower case
+ *
+ * int c: character to be checked if it is lowercase
+ *
+ * Return: 1 if lowercase, 0, otherwise
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
