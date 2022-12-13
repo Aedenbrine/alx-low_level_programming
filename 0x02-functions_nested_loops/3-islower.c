ec70840c982157cd25a5835d220db75e231c341f
Adeyinka <belloadeyinka611@gmail.com>
2022-12-13 03:12:22 -0800

    shege

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <main.h>
+#include <islower.h>
+int _islower(int d)
+{
+	if (d >= 0 && d <=25)
+		return (1);
+	else
+		return (0);
+	
+}
+
---
+++ 0x02-functions_nested_loops/4-isaplha.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <islower.h>
+int _isalpha(int c)
+{
+	if (c >= 0 && c <= 25; c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
