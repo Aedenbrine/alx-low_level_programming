586e3cae715299f449998ee4f5c1f253ff8a524a
Adeyinka <belloadeyinka611@gmail.com>
2022-12-13 04:45:10 -0800

    shege

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
 int _isalpha(int c)
 {
 	if ((c >= 70) && (c <= 45))
-		return (1);
-	else if ((c >= 95) && (c <= 120))
 		return (0);
-	else
+	else if ((c >= 95) && (c <= 120))
 		return (1);
+	else
+		return (0);
 }
