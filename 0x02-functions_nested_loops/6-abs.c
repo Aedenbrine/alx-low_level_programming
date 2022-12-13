ca28a7afc38b2a226736b0e9df6d20f070d128a5
Adeyinka <belloadeyinka611@gmail.com>
2022-12-13 09:33:56 -0800

    shege

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abt - A finction that print the absolute value of an integer
+ *
+ * @int: integer to be printed
+ *
+ * Return: n if interger is gretera than 0, otherwise return -n
+ */
+
+int _abt(int)
+{
+	if (n > 0)
+	{
+		return (n)
+	}
+	else
+	{
+		return (-n)
+	}
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints all minute of the day
+ * @void: Paramters to add
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hr;
+	int min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			putchar((hr / 10)+ '0');
+			putchar((hr % 10)+ '0');
+			putchar(':');
+			putchar((min / 10)+ '0');
+			putchar((min % 10)+ '0');
+			putchar('\n')
+		}
+	}
+}
