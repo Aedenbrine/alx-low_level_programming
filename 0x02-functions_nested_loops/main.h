d1f44f55c5f7c7b72469b99c7bb80aa8379e23c1
Adeyinka <belloadeyinka611@gmail.com>
2022-12-13 04:04:31 -0800

    shege

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,22 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+void num(int r, int n);
+void print_times_table(int n);
+void print_to_98(int n);
+int add(int a, int b);
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+
+#endif
