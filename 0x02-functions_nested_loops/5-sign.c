5ee1e219d3a66d56856e1baa12399ffd69ec838c
Adeyinka <belloadeyinka611@gmail.com>
2022-12-13 05:00:26 -0800

    shege

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,2 +1,21 @@
 #include "main.h"
 
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf('+');
+		return (1);
+	}
+	else (n > 0)
+	{
+		printf('-');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf(0);
+		return (0);
+	}
+
+}
