5c655f841431ea71e1b665bd8cb77f1ffe33e160
Adeyinka <belloadeyinka611@gmail.com>
2022-12-14 04:40:22 -0800

    shege

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -44,8 +44,8 @@ void print_times_table(int n)
 					}
 					else if (k >= 100)
 					{
-						putchar(' ');
 						putchar(',');
+						putchar(' ');
 						putchar((k / 100) + '0');
 						putchar(((k / 10) % 10) + '0');
 						putchar((k % 10) + '0');
