7329d9c6c3599f07783617dea7da07e71a897134
Adeyinka <belloadeyinka611@gmail.com>
2022-12-19 03:56:34 -0800

    shege

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+void_putchar(char *str)
+{
+	int = a;
+
+	for (i = 0; str[a] != '\0'; 1++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#inlcude "main.h"
+void print_rev(char *s)
+{
+	int i = 0;
+	int lenth;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
