499163a2d4c46a47c64d052c66e0f769afbbe6d9
Adeyinka <belloadeyinka611@gmail.com>
2022-12-22 12:51:04 -0800

    shege

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,23 +3,14 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char *n1 = "12345678923445678901234567881234";
-	char *n2 = "98741232318193298928392938392939";
-	char *r[100];
-	int size_r[10]
+	char first, second, *n1, *n2, sum;
 
-		res = infinite_add(n1, n2, r, 100);
-	if (res == 0)
-	{
-		printf("error\n");
-	}
-	else
-	{
-		printf("%s + %s = %s\n",  n1, n2, res);
-	}
-	n1 = "1234567890";
-	n2 = "1";
-	res = infinite_add(n1, n2);
+	printf("%d%d", &first, &second);
+
+	n1 = &first;
+	n2 = &second;
+
+	sum = *n1 + *n2;
+	printf("sum of the numbers = %d\n", sum);
 	return (0);
 }
-
