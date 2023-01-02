90eb9cb947337af4d3c96e88258f122f15987647
Adeyinka <belloadeyinka611@gmail.com>
2023-01-02 05:19:41 -0800

    shege

--- 0x07-pointers_arrays_strings/8-print_diagsum.c
+++ 0x07-pointers_arrays_strings/8-print_diagsum.c
@@ -1,47 +1,20 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_diagsums -  prints sums
+ * @a: a pointer
+ * @size: size
  */
-
-int main(void)
-
+void print_diagsums(int *a, int size)
 {
+	int i, sum1 = 0, sum2 = 0;
 
-    int c3[3][3] = {
-
-        {0, 1, 5},
-
-        {10, 11, 12},
-
-        {1000, 101, 102},
-
-    };
-
-    int c5[5][5] = {
-
-        {0, 1, 5, 12124, 1234},
-
-        {10, 11, 12, 123521, 12512},
-
-        {1000, 101, 102, 12545, 214543435},
-
-        {100, 1012451, 11102, 12545, 214543435},
-
-        {10, 12401, 10452, 11542545, 1214543435},
-
-    };
-
-    print_diagsums((int *)c3, 3);
-
-    print_diagsums((int *)c5, 5);
-
-    return (0);
-
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
 }
